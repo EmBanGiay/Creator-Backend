bd837c2142cc86e13c46bd1d54bb4b7fdeaef3b6
Vazy <86739701+ZzVAZYzZ@users.noreply.github.com>
2024-06-05 22:11:21 +0700

    add systemStarter

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 const express = require('express');
-const axios = require('axios');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const connectDb = require('./configs/dbConnection');
 const errorHandler = require('./middlewares/errorHandler');
 
+const systemStart = require('./start/systemStarter') 
 const tokenRoutes = require('./routes/tokenRoute');
 const recordRoutes = require('./routes/recordRoute');
 
@@ -19,9 +19,6 @@ app.use('/api', tokenRoutes);
 app.use('/api', recordRoutes);
 app.use(errorHandler);
 
-(function () {
-    axios.post('http://localhost:5001/api/generateToken');
-})();
 
 app.listen(5001, () => {
     console.log('Listening on 5001');
---
+++ start/systemStarter.js
@@ -0,0 +1,5 @@
+const axios = require('axios');
+
+(function () {
+    axios.post('http://localhost:5001/api/generateToken');
+})();
